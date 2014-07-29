1b148c00f7a3e3332b07f210c3d81df0d8b7666a
Sudhanshu patel <sudhanshuptl13@gmail.com>
2014-07-29 20:39:58 +0530

    v 1.1
    it working but file handling and time steel missing

--- Port Scanner/Port Scanner.py
+++ Port Scanner/Port Scanner.py
@@ -1,68 +1,49 @@
 # ___________MAIN MODULE___________________
-#  V-1.0
+#  V-1.1
 # Port Scanner 
 __author__ = "Sudhanshu Patel (sudhanshuptl13@gmail.com)"
 
-import subprocess
-from datetime import datetime
-import socket ,sys
-
-class portscan():
-    def __init__(self,remoteServer):
-        '''Initiate port scanner'''
-        self.remoteServer=remoteServer
-        self._port_ls=[21, 22, 23, 25, 53, 53, 80, 81, 110, 111,119, 123, 135, 139, 143, 161, 443, 445,465,563,587,902,912,993,995, 1024, 1723, 3389, 4567, 5000,5631, 8080, 8081,9601]
-        # Some of most common port
-        try:
-            self.remoteServerIP=socket.gethostbyname(remoteServer) #Resolving IP of host
-        except:
-            print "Unable to fetch Host"
-
-    def scanport(self):
-        '''
-           Using Range Function to specify ports (here We scan all port in range 1 to 1024
-           we also create some Error handler to handle Error
-        '''
-        print "be patience , it will take time..."
-        try:  #Exception Handling
-            for port in range(1,1024):#self._port_ls:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                result = sock.connect_ex((self.remoteServerIP, port))
-                if result == 0:  #If port is open(i.e using this port we can connect to host )
-                    print "Port {}: \t Open".format(port) #print port
-                sock.close() #close Socket
-        except KeyboardInterrupt:
-            print "You pressed Ctrl+C"
-            sys.exit()
-
-        except socket.gaierror:
-            print 'Hostname could not be resolved. Exiting'
-            sys.exit()
-        except socket.error:
-            print "Couldn't connect to server"
-            sys.exit()
-        
-
-
+import sys
+try:
+    sys.path.insert(0,'sub_program')
+    import scport
+except:
+    print 'Unable to Find subprogram folder or scport.py'
 
 if __name__=='__main__':
-    
-    #Clear Screen
-    #subprocess.call('clear',shell=True)
-    
-    # Information for User
+     # Information for User
     print "Enter a remote host to scane Ex:- www.nitrkl.ac.in\n"+"-"*60
     print (" "*29)+"OR\n"+"_"*60
-    print "Enter a remote computer name in your local network  Ex:- Jai_Hind\n"+"-"*60
+    print "Enter a remote computer name in your local network  Ex:- Sudhanshu\n"+"-"*60
 
-    #Ask for input and create object scan
-    scan=portscan(raw_input('Enter a remote host to scan : '))
+    while True:
+        print """
+                1 ::- Scan remote Host for most common used Port
+                2 ::- Brute Scan : Scan host for all port in range 0 to 1024
+                3 ::- Scan remote host for port you want (you have to enter port)
+                4 ::- Exit !!
+                 """
+        choice=raw_input('Enter Your Choice ::- ')
+        if choice=='1':
+            ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
+            ls=ob.Scan_common_port()
+            
+        elif choice=='2':
+            ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
+            ls=ob.Brute_scan()
+            
+        elif choice=='3':
+            ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
+            print 'Enter port address seperated by space on which scan is going to perform :'
+            ls= ob.Given_Portscan([int(x) for x in raw_input().split(' ')])
+            
+        elif choice=='4':
+            print 'Thank You !'
+            sys.exit()
+            
+        else:
+            print "Wrong Choice Try again"
+        
+       
+ 
 
-    # Scanning Port and printing Result
-    scan.scanport()
-    print """
-                        We r Done !!
-                          Thank You!!"
-                        """
-    raw_input()
-    
