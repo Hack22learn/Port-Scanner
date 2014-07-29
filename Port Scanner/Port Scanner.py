71adec23f77f17f4324bc4a9d651896aece23d3a
Sudhanshu patel <sudhanshuptl13@gmail.com>
2014-07-29 17:37:15 +0530

    modified for some most common port

--- Port Scanner/Port Scanner.py
+++ Port Scanner/Port Scanner.py
@@ -11,6 +11,8 @@ class portscan():
     def __init__(self,remoteServer):
         '''Initiate port scanner'''
         self.remoteServer=remoteServer
+        self._port_ls=[21, 22, 23, 25, 53, 53, 80, 81, 110, 111,119, 123, 135, 139, 143, 161, 443, 445,465,563,587,902,912,993,995, 1024, 1723, 3389, 4567, 5000,5631, 8080, 8081,9601]
+        # Some of most common port
         try:
             self.remoteServerIP=socket.gethostbyname(remoteServer) #Resolving IP of host
         except:
@@ -23,7 +25,7 @@ class portscan():
         '''
         print "be patience , it will take time..."
         try:  #Exception Handling
-            for port in range(1,1024):
+            for port in range(1,1024):#self._port_ls:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 result = sock.connect_ex((self.remoteServerIP, port))
                 if result == 0:  #If port is open(i.e using this port we can connect to host )
@@ -46,7 +48,7 @@ class portscan():
 if __name__=='__main__':
     
     #Clear Screen
-    subprocess.call('clear',shell=True)
+    #subprocess.call('clear',shell=True)
     
     # Information for User
     print "Enter a remote host to scane Ex:- www.nitrkl.ac.in\n"+"-"*60
@@ -54,8 +56,13 @@ if __name__=='__main__':
     print "Enter a remote computer name in your local network  Ex:- Jai_Hind\n"+"-"*60
 
     #Ask for input and create object scan
-    scan=portscan(raw_input('Enter a remote host to scan:'))
+    scan=portscan(raw_input('Enter a remote host to scan : '))
 
     # Scanning Port and printing Result
     scan.scanport()
+    print """
+                        We r Done !!
+                          Thank You!!"
+                        """
+    raw_input()
     
