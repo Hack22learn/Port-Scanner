1dcbf9a898ea5bba116a3f6e470e0fe493ec9db6
Sudhanshu patel <sudhanshuptl13@gmail.com>
2014-08-01 10:28:23 +0530

    V 1.02
    Short handing code ,adding few more exception handler, we need to focus
    on case if host not found don't call function to scan

--- Port Scanner/Port Scanner.py
+++ Port Scanner/Port Scanner.py
@@ -1,6 +1,5 @@
 # ___________MAIN MODULE___________________
-#  V-1.1
-#Python 2.7.6
+#  V-1.02
 # Port Scanner 
 __author__ = "Sudhanshu Patel (sudhanshuptl13@gmail.com)"
 
@@ -18,34 +17,39 @@ if __name__=='__main__':
     print "Enter a remote computer name in your local network  Ex:- Sudhanshu\n"+"-"*60
 
     while True:
-        print """
+        try:
+            print """
                 1 ::- Scan remote Host for most common used Port
                 2 ::- Brute Scan : Scan host for all port in range 0 to 1024
                 3 ::- Scan remote host for port you want (you have to enter port)
                 4 ::- Exit !!
                  """
-        choice=raw_input('Enter Your Choice ::- ')
-        if choice=='1':
-            ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
-            ls=ob.Scan_common_port()
+            choice=raw_input('Enter Your Choice ::- ')
+            if choice=='1':
+                ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
+                ls=ob.Scan_port()
             #we use ls for to store it in file
             
-        elif choice=='2':
-            ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
-            ls=ob.Brute_scan()
+            elif choice=='2':
+                ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
+                ls=ob.Brute_scan()
             
-        elif choice=='3':
-            ob=scport.Portscan(raw_input('Enter Remote Host name :- '))
-            print 'Enter port address seperated by space on which scan is going to perform :'
-            ls= ob.Given_Portscan([int(x) for x in raw_input().split(' ')])
-            
-        elif choice=='4':
-            print 'Thank You !'
-            sys.exit()
-            
-        else:
-            print "Wrong Choice Try again"
+            elif choice=='3':
+                print 'Enter port address seperated by space on which scan is going to perform :'
+                ls=[int(x) for x in raw_input().split(' ')]
+                ob=scport.Portscan(raw_input('Enter Remote Host name :- '),ls)
+                ls= ob.Scan_port()
         
-       
+            
+            elif choice=='4':
+                print 'Thank You !'
+                sys.exit()
+            
+            else:
+                print "Wrong Choice Try again"
+        
+        except KeyboardInterrupt:
+            print 'You Pressed Ctrl+C'
+            sys.exit()
  
 
--- Port Scanner/sub_program/scport.py
+++ Port Scanner/sub_program/scport.py
@@ -2,11 +2,11 @@ from datetime import datetime
 import socket,sys 
 
 class Portscan():
-    def __init__(self,remoteServer):
+    # Some of most common port is default argument
+    def __init__(self,remoteServer,df_list=[21, 22, 23, 25, 53, 53, 80, 81, 110, 111,119, 123, 135, 139, 143, 161, 443, 445,465,563,587,902,912,993,995, 1024, 1723, 3389, 4567, 5000,5631, 8080, 8081,9601]):
         '''Initiate port scanner'''
         self.remoteServer=remoteServer
-        self._port_ls=[21, 22, 23, 25, 53, 53, 80, 81, 110, 111,119, 123, 135, 139, 143, 161, 443, 445,465,563,587,902,912,993,995, 1024, 1723, 3389, 4567, 5000,5631, 8080, 8081,9601]
-        # Some of most common port
+        self._port_ls= df_list
         try:
             self.remoteServerIP=socket.gethostbyname(remoteServer) #Resolving IP of host
             print '*'*80
@@ -14,9 +14,10 @@ class Portscan():
             print '*'*80
         except:
             print "Unable to fetch Host"
+            sys.exit() #we need to detroy object we have to find out a way to do this
             
 
-    def Scan_common_port(self):
+    def Scan_port(self):
         '''
            We check for all commonly used  port .....
         '''
@@ -26,11 +27,14 @@ class Portscan():
         output_port=[]
         try:  #Exception Handling
             for port in self._port_ls:
+                print 'Checking for Port ->> ',port
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 result = sock.connect_ex((self.remoteServerIP, port))
                 if result == 0:  #If port is open(i.e using this port we can connect to host )
-                    print "Port {}: \t Open".format(port) #print port
+                    print "\t\t\t\t Open\n"
                     output_port.append(port)
+                else:
+                    print "\t\t\t Closed\n"
                 sock.close() #close Socket
 
             print ' \n        We r Done!!  '
@@ -59,12 +63,15 @@ class Portscan():
 
         output_port=[]
         try:  #Exception Handling
-            for port in range(0,1025):
+            for port in range(1,1025):
+                print 'Checking for Port ->> ',port
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 result = sock.connect_ex((self.remoteServerIP, port))
                 if result == 0:  #If port is open(i.e using this port we can connect to host )
-                    print "Port {}: \t Open".format(port) #print port
+                    print "\t\t\t\t Open\n"
                     output_port.append(port)
+                else:
+                     print "\t\t\t Closed\n" 
                 sock.close() #close Socket
 
             print ' \n        We r Done!!  '
@@ -82,36 +89,3 @@ class Portscan():
             print "Couldn't connect to server"
             sys.exit()
 
-    def Given_Portscan(self,ls=[1]):
-        '''
-           Using Range Function to specify ports (here We scan all port in range 1 to 1024
-           we also create some Error handler to handle Error
-        '''
-        print "....................Be Patience , it will take time.................... "
-        print ' We Gonna Check for All port in range 0 to 1024\n '
-
-        output_port=[]
-        try:  #Exception Handling
-            for port in ls:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                result = sock.connect_ex((self.remoteServerIP, port))
-                if result == 0:  #If port is open(i.e using this port we can connect to host )
-                    print "Port {}: \t Open".format(port) #print port
-                    output_port.append(port)
-                sock.close() #close Socket
-
-            print ' \n        We r Done!! \n '
-            return output_port
-                
-                
-        except KeyboardInterrupt:
-            print "You pressed Ctrl+C"
-            sys.exit()
-
-        except socket.gaierror:
-            print 'Hostname could not be resolved. Exiting'
-            sys.exit()
-        except socket.error:
-            print "Couldn't connect to server"
-            sys.exit()
-            
